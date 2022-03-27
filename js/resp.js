f258d5154d289e2a49c81b0ca1cf62dbdb86b56c
Nikhil <lcs2021006@iiitl.ac.in>
2022-03-27 10:19:28 +0530

    Initial commit

---
+++ js/resp.js
@@ -0,0 +1,9 @@
+let burger = document.querySelector('.burger')
+let navbar = document.querySelector('.navbar')
+let navList = document.querySelector('.nav-list')
+let rightNav = document.querySelector('.rightNav')
+burger.addEventListener('click',()=>{
+    rightNav.classList.toggle('v-class-resp')
+    navList.classList.toggle('v-class-resp')
+    navbar.classList.toggle('h-nav-resp')
+})
